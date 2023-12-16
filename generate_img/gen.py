b9078b65f984d8d5e20430aa2400b8a01769c1b2
Rimvydas Civilis <rimvydascivilis@proton.me>
2023-12-16 17:59:01 +0200

    Add image generation script

---
+++ generate_img/gen.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+
+NO_MATERIALS = 2
+items = ['helmet', 'armor', 'shoes', 'sword']
+
+directory_name = "res"
+if not os.path.isdir(directory_name):
+  os.mkdir(directory_name)
+
+for i in range(NO_MATERIALS):
+  for j in range(NO_MATERIALS):
+    for k in range(NO_MATERIALS):
+      for l in range(NO_MATERIALS):
+        index = [i, j, k, l]
+        finalImage = Image.open(f'img/{items[0]}_{index[0]}.png')
+        for m in range(1, len(items)):
+          if l == 0 and items[m] == 'sword': 
+            continue
+          layer = Image.open(f'img/{items[m]}_{index[m]}.png')
+          finalImage.paste(layer, (0, 0), layer)
+
+        finalImage.save(f'res/{i}_{j}_{k}_{l}.png')
