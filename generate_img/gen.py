1623663629d76c9ec33d228319b684655cf4240a
Rimvydas Civilis <rimvydascivilis@proton.me>
2023-12-20 19:38:04 +0200

    Add all character parts

--- generate_img/gen.py
+++ generate_img/gen.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import os
 
-NO_MATERIALS = 2
-items = ['helmet', 'armor', 'shoes', 'sword']
+NO_MATERIALS = 5
+items = ['helmet', 'armor', 'shoes', 'weapon']
 
 directory_name = "res"
 if not os.path.isdir(directory_name):
@@ -15,7 +15,7 @@ for i in range(NO_MATERIALS):
         index = [i, j, k, l]
         finalImage = Image.open(f'img/{items[0]}_{index[0]}.png')
         for m in range(1, len(items)):
-          if l == 0 and items[m] == 'sword': 
+          if l == 0 and items[m] == 'weapon': 
             continue
           layer = Image.open(f'img/{items[m]}_{index[m]}.png')
           finalImage.paste(layer, (0, 0), layer)
